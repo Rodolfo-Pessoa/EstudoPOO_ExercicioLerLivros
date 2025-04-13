96ee28bc8b98544802e46ca1170f42118b0b3d44
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-13 18:34:07 -0300

    Primeiro commit

---
+++ src/Pessoa/Livros.java
@@ -0,0 +1,12 @@
+package Pessoa;
+
+public class LerLivros {
+
+    private String titulo, autor, leitor;
+    private int totPaginas;
+    private boolean aberto;
+
+    public void detalhes() {
+
+    }
+}
